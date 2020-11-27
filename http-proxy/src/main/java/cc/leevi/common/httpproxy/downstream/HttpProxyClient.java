1b078b4c9c311f9f26b21104dbcc9345625dc523
jiangxin <jiangxin@95303.com>
2020-11-27 16:53:02 +0800

    临时提交

--- http-proxy/src/main/java/cc/leevi/common/httpproxy/downstream/HttpProxyClient.java
+++ http-proxy/src/main/java/cc/leevi/common/httpproxy/downstream/HttpProxyClient.java
@@ -3,11 +3,18 @@ package cc.leevi.common.httpproxy.downstream;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.Channel;
+import io.netty.channel.EventLoop;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.FullHttpMessage;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class HttpProxyClient {
 
@@ -15,29 +22,59 @@ public class HttpProxyClient {
 
     private Bootstrap clientBootstrap;
 
-    private EventLoopGroup clientEventLoopGroup;
-
     private volatile int status = 0;
 
-    private Channel acceptorChannel;
+    private String host;
+
+    private int port;
+
+    private EventLoop upstreamEventLoop;
+
+    private FullHttpRequest httpRequest;
+
+    public HttpProxyClient(EventLoop upstreamEventLoop, FullHttpRequest httpRequest) {
+        this.upstreamEventLoop = upstreamEventLoop;
+        this.httpRequest = httpRequest;
+    }
+
+    private void parseURI(FullHttpRequest httpRequest) throws URISyntaxException {
+        String uriPlain = httpRequest.uri();
+        URI uri = new URI(uriPlain);
+        this.host = uri.getHost();
+        this.port = uri.getPort();
+    }
 
 
-    public void startServer(){
+    public void connectAndWrite(){
+        try {
+            parseURI(httpRequest);
+
+            clientBootstrap = new Bootstrap()
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new HttpProxyClientInitializer())
+                    .group(upstreamEventLoop);
+            Channel channel = clientBootstrap.connect(host, port).syncUninterruptibly().channel();
+            
+
+        } catch (URISyntaxException e) {
+            logger.error("URI syntax illegal: "+ httpRequest.uri(),e);
+        }
         logger.info("Proxy Client starting...");
 
-        clientEventLoopGroup = new NioEventLoopGroup(4);
 
-        clientBootstrap = new Bootstrap()
-                .channel(NioServerSocketChannel.class)
-                .handler(new HttpProxyClientInitializer())
-                .group(clientEventLoopGroup);
-        acceptorChannel = clientBootstrap.connect();
+
+//        httpRequest.
+
+
+//        channel.writeAndFlush()
+
+
     }
 
-    public void shutdown(){
-        logger.info("Proxy Client shutting down...");
-        acceptorChannel.close().syncUninterruptibly();
-        clientEventLoopGroup.shutdownGracefully().syncUninterruptibly();
-        logger.info("shutdown completed!");
-    }
+//    public void shutdown(){
+//        logger.info("Proxy Client shutting down...");
+//        acceptorChannel.close().syncUninterruptibly();
+//        clientEventLoopGroup.shutdownGracefully().syncUninterruptibly();
+//        logger.info("shutdown completed!");
+//    }
 }
--- http-proxy/src/main/java/cc/leevi/common/httpproxy/upstream/HttpProxyServerHandler.java
+++ http-proxy/src/main/java/cc/leevi/common/httpproxy/upstream/HttpProxyServerHandler.java
@@ -1,23 +1,30 @@
 package cc.leevi.common.httpproxy.upstream;
 
+import cc.leevi.common.httpproxy.downstream.HttpProxyClient;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.EventLoop;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.FullHttpMessage;
+import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpObject;
 import io.netty.handler.codec.http.HttpRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.net.URI;
 
 public class HttpProxyServerHandler extends SimpleChannelInboundHandler<HttpObject> {
 
     private Logger logger = LoggerFactory.getLogger(HttpProxyServerHandler.class);
 
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, HttpObject httpObject) throws Exception {
-        if(httpObject instanceof HttpRequest){
-            HttpRequest httpRequest = (HttpRequest)httpObject;
-            String uri = httpRequest.uri();
+        if(httpObject instanceof FullHttpMessage){
+            FullHttpRequest httpRequest = (FullHttpRequest)httpObject;
+            String uriPlain = httpRequest.uri();
             // TODO: 2020/11/26 解析url，dns之类，拿到host & port，然后连接，使用httpProxyClient
-
-            logger.info(uri);
+            EventLoop upstreamEventLoop = channelHandlerContext.channel().eventLoop();
+            HttpProxyClient httpProxyClient = new HttpProxyClient(upstreamEventLoop, httpRequest);
+            httpProxyClient.connectAndWrite();
         }
     }
 }
