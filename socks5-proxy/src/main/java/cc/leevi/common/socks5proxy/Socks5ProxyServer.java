32c3934ac8bd24dbd45229e069ba21792403f2ea
jiangxin <jiangxin@95303.com>
2020-12-16 17:45:27 +0800

    临时提交

---
+++ socks5-proxy/src/main/java/cc/leevi/common/socks5proxy/Socks5ProxyServer.java
@@ -0,0 +1,36 @@
+package cc.leevi.common.socks5proxy;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Socks5ProxyServer {
+    private Logger logger = LoggerFactory.getLogger(Socks5ProxyServer.class);
+
+    private ServerBootstrap serverBootstrap;
+
+    private EventLoopGroup serverEventLoopGroup;
+
+    private Channel acceptorChannel;
+
+    public void startServer(){
+        logger.info("Proxy Server starting...");
+
+        serverEventLoopGroup = new NioEventLoopGroup(4);
+
+        serverBootstrap = new ServerBootstrap()
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new HttpProxyServerInitializer())
+                .group(serverEventLoopGroup);
+        acceptorChannel = serverBootstrap.bind(8080).syncUninterruptibly().channel();
+    }
+
+    public void shutdown(){
+        logger.info("Proxy Server shutting down...");
+        acceptorChannel.close().syncUninterruptibly();
+        serverEventLoopGroup.shutdownGracefully().syncUninterruptibly();
+        logger.info("shutdown completed!");
+    }
+}
---
+++ socks5-proxy/src/main/java/cc/leevi/common/socks5proxy/SocksServerHandler.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package cc.leevi.common.socks5proxy;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.socksx.SocksMessage;
+import io.netty.handler.codec.socksx.SocksVersion;
+import io.netty.handler.codec.socksx.v4.Socks4CommandRequest;
+import io.netty.handler.codec.socksx.v4.Socks4CommandType;
+import io.netty.handler.codec.socksx.v5.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@ChannelHandler.Sharable
+public final class SocksServerHandler extends SimpleChannelInboundHandler<SocksMessage> {
+
+    private Logger logger = LoggerFactory.getLogger(SocksServerHandler.class);
+
+    public static final SocksServerHandler INSTANCE = new SocksServerHandler();
+
+    private SocksServerHandler() { }
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, SocksMessage socksRequest) throws Exception {
+        if(!socksRequest.version().equals(SocksVersion.SOCKS5)){
+            logger.error("only supports socks5 protocol!");
+            ctx.writeAndFlush(Unpooled.wrappedBuffer("protocol version illegal!".getBytes()));
+            return ;
+        }
+        if (socksRequest instanceof Socks5InitialRequest) {
+            ctx.pipeline().addFirst(new Socks5CommandRequestDecoder());
+            ctx.write(new DefaultSocks5InitialResponse(Socks5AuthMethod.NO_AUTH));
+        } else if (socksRequest instanceof Socks5PasswordAuthRequest) {
+            ctx.pipeline().addFirst(new Socks5CommandRequestDecoder());
+            ctx.write(new DefaultSocks5PasswordAuthResponse(Socks5PasswordAuthStatus.SUCCESS));
+        } else if (socksRequest instanceof Socks5CommandRequest) {
+            Socks5CommandRequest socks5CmdRequest = (Socks5CommandRequest) socksRequest;
+            if (socks5CmdRequest.type() == Socks5CommandType.CONNECT) {
+                ctx.pipeline().addLast(new SocksServerConnectHandler());
+                ctx.pipeline().remove(this);
+                ctx.fireChannelRead(socksRequest);
+            } else {
+                ctx.close();
+            }
+        } else {
+            ctx.close();
+        }
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable throwable) {
+        logger.error("exceptionCaught",throwable);
+        SocksServerUtils.closeOnFlush(ctx.channel());
+    }
+}
